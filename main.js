9b1358e3ebbfc284ed66ae77645c17144c1509a2
Satyajit Mishra <55646021+smish-hash@users.noreply.github.com>
2020-06-30 23:38:41 +0530

    My first website?

---
+++ main.js
@@ -0,0 +1,22 @@
+function clock() {
+	
+	var hours = document.getElementById('hour');
+	var minutes = document.getElementById('minute');
+	var seconds = document.getElementById('second');
+
+	var h = new Date().getHours();
+	var m = new Date().getMinutes();
+	var s = new Date().getSeconds();
+
+	h = (h<10)?"0"+h :h
+	m = (m<10)?"0"+m :m
+	s = (s<10)?"0"+s :s
+
+
+	hours.innerHTML = h;
+	minutes.innerHTML = m;
+	seconds.innerHTML = s;
+
+}
+
+var interval = setInterval(clock,1000);
